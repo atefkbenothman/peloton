dcbdf215b881f9c51430c2cbaf2332c886dbb026
atefkbenothman <atefkbenothman@gmail.com>
2023-02-14 11:44:38 -0800

    add login page

---
+++ strava-ui/src/pages/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto flex flex-col items-center">
+          <h3 className="text-3xl font-bold">Login with Strava</h3>
+          <button className="btn bg-orange-500 rounded m-8 p-2 font-bold">Login</button>
+        </div>
+      </div>
+    </>
+  )
+}
