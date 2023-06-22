ef5c57714926ac902d4b7a4c768a9deadb0ad199
atefkbenothman <atefkbenothman@gmail.com>
2023-06-22 16:20:42 -0700

    update navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,191 +10,100 @@ function NavBar() {
 
   return (
     <>
-      <div className="my-40 mx-1">
-        <ul
-          className="
-            flex 
-            list-none 
-            flex-col 
-            flex-wrap 
-          "
-          role="tablist"
-          data-te-nav-ref>
-          <li role="presentation" className="flex-grow text-center">
-            <a
-              className="
-                cursor-pointer
-                shadow-md
-                my-2 
-                block 
-                rounded 
-                px-7 
-                pt-4 
-                pb-3.5 
-                text-xs 
-                font-bold 
-                uppercase 
-                leading-tight 
-                text-neutral-500 
-                data-[te-nav-active]:!bg-blue-300 
-                dark:bg-neutral-400 
-                dark:text-white 
-                dark:data-[te-nav-active]:text-white-700
-              "
-              id="pills-home-tab03"
-              data-te-toggle="pill"
-              data-te-target="#pills-home03"
-              data-te-nav-active
-              role="tab"
-              aria-controls="pills-home03"
-              aria-selected="true"
-              onClick={() => goToPage("/")}
-            >Home</a>
-          </li>
-          <li role="profile" className="flex-grow text-center">
-            <a
-              className="
-                cursor-pointer
-                shadow-md
-                my-2 
-                block 
-                rounded 
-                px-7 
-                pt-4 
-                pb-3.5 
-                text-xs 
-                font-bold 
-                uppercase 
-                leading-tight 
-                text-neutral-500 
-                data-[te-nav-active]:!bg-blue-300 
-                dark:bg-neutral-400 
-                dark:text-white 
-                dark:data-[te-nav-active]:text-white-700
-              "
-              id="pills-profile-tab03"
-              data-te-toggle="pill"
-              data-te-target="#pills-profile03"
-              role="tab"
-              aria-controls="pills-profile03"
-              aria-selected="false"
-              onClick={() => goToPage("/login")}
-            >Login</a>
-          </li>
-          <li role="contact" className="flex-grow text-center">
-            <a
-              className="
-                cursor-pointer
-                shadow-md
-                my-2 
-                block 
-                rounded 
-                px-7 
-                pt-4 
-                pb-3.5 
-                text-xs 
-                font-bold
-                uppercase 
-                nav-link
-                leading-tight 
-                text-neutral-500 
-                data-[te-nav-active]:!bg-blue-300 
-                dark:bg-neutral-400 
-                dark:text-white 
-                dark:data-[te-nav-active]:text-white-700
-              "
-              id="pills-contact-tab03"
-              data-te-toggle="pill"
-              data-te-target="#pills-contact03"
-              role="tab"
-              aria-controls="pills-contact03"
-              aria-selected="false"
-              onClick={() => goToPage("/activities")}
-            >Activities</a>
-          </li>
-          <li role="profile" className="flex-grow text-center">
-            <a
-              className="
-                cursor-pointer
-                shadow-md
-                my-2 
-                block 
-                rounded 
-                px-7 
-                pt-4 
-                pb-3.5 
-                text-xs 
-                font-bold 
-                uppercase 
-                leading-tight 
-                text-neutral-500 
-                data-[te-nav-active]:!bg-blue-300 
-                dark:bg-neutral-400 
-                dark:text-white 
-                dark:data-[te-nav-active]:text-white-700
-              "
-              id="pills-profile-tab03"
-              data-te-toggle="pill"
-              data-te-target="#pills-profile03"
-              role="tab"
-              aria-controls="pills-profile03"
-              aria-selected="false"
-              onClick={() => goToPage("/kom")}
-            >KOM</a>
-          </li>
-          <li role="profile" className="flex-grow text-center">
-            <a
-              className="
-                cursor-pointer
-                shadow-md
-                my-2 
-                block 
-                rounded 
-                px-7 
-                pt-4 
-                pb-3.5 
-                text-xs 
-                font-bold 
-                uppercase 
-                leading-tight 
-                text-neutral-500 
-                data-[te-nav-active]:!bg-blue-300 
-                dark:bg-neutral-400 
-                dark:text-white 
-                dark:data-[te-nav-active]:text-white-700
-              "
-              id="pills-profile-tab03"
-              data-te-toggle="pill"
-              data-te-target="#pills-profile03"
-              role="tab"
-              aria-controls="pills-profile03"
-              aria-selected="false"
-              onClick={() => goToPage("/stats")}
-            >Stats</a>
-          </li>
-        </ul>
-        <div className="my-2">
-          <div
-            className="hidden opacity-0 opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-            id="pills-home03"
-            role="tabpanel"
-            aria-labelledby="pills-home-tab03"
-            data-te-tab-active>
+
+      {/* Sidebar Button */}
+      <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <span className="sr-only">Open sidebar</span>
+        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+      </button>
+
+      {/* Sidebar */}
+      <aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-800">
+
+          {/* Logo */}
+          <div className="flex items-center mb-2">
+            <img className="" src="/strava-ar21.svg" alt="Strava Logo" />
           </div>
-          <div
-            className="hidden opacity-0 opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-            id="pills-profile03"
-            role="tabpanel"
-            aria-labelledby="pills-profile-tab03">
-          </div>
-          <div
-            className="hidden opacity-0 opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-            id="pills-contact03"
-            role="tabpanel"
-            aria-labelledby="pills-contact-tab03">
-          </div>
+
+          <ul className="space-y-3 font-semibold text-lg">
+
+            {/* Home */}
+            <li>
+              <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
+              </svg>
+                <span className="ml-3">Home</span>
+              </a>
+            </li>
+
+            {/* Login */}
+            <li>
+              <a href="/login" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M20 10a28.076 28.076 0 0 1-1.091 9M6.231 2.37a8.994 8.994 0 0 1 12.88 3.73M1.958 13S2 12.577 2 10a8.949 8.949 0 0 1 1.735-5.307m12.84 3.088c.281.706.426 1.46.425 2.22a30 30 0 0 1-.464 6.231M5 10a6 6 0 0 1 9.352-4.974M3 19a5.964 5.964 0 0 1 1.01-3.328 5.15 5.15 0 0 0 .786-1.926m8.66 2.486a13.96 13.96 0 0 1-.962 2.683M6.5 17.336C8 15.092 8 12.846 8 10a3 3 0 1 1 6 0c0 .75 0 1.521-.031 2.311M11 10.001c0 3 0 6-2 9"/>
+              </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Login</span>
+              </a>
+            </li>
+
+            {/* Profile */}
+            <li>
+                <a href="/stats" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
+                </svg>
+                  <span className="flex-2 ml-3 whitespace-nowrap">Profile</span>
+                </a>
+            </li>
+
+            {/* Activities */}
+            <li>
+              <a href="/activities" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M6.487 1.746c0 4.192 3.592 1.66 4.592 5.754 0 .828 1 1.5 2 1.5s2-.672 2-1.5a1.5 1.5 0 0 1 1.5-1.5h1.5m-16.02.471c4.02 2.248 1.776 4.216 4.878 5.645C10.18 13.61 9 19 9 19m9.366-6h-2.287a3 3 0 0 0-3 3v2m6-8a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+              </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Activities</span>
+              </a>
+            </li>
+            
+            {/* KOM */}
+            <li>
+              <a href="/kom" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="m8.806 5.614-4.251.362-2.244 2.243a1.058 1.058 0 0 0 .6 1.8l3.036.356m9.439 1.819-.362 4.25-2.243 2.245a1.059 1.059 0 0 1-1.795-.6l-.449-2.983m9.187-12.57a1.536 1.536 0 0 0-1.26-1.26c-1.818-.313-5.52-.7-7.179.96-1.88 1.88-5.863 9.016-7.1 11.275a1.05 1.05 0 0 0 .183 1.25l.932.939.937.936a1.049 1.049 0 0 0 1.25.183c2.259-1.24 9.394-5.222 11.275-7.1 1.66-1.663 1.275-5.365.962-7.183Zm-3.332 4.187a2.115 2.115 0 1 1-4.23 0 2.115 2.115 0 0 1 4.23 0Z"/>
+              </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">KOM</span>
+              </a>
+            </li>
+
+          </ul>
+          
+
+          {/* Seperator */}
+          <ul className="pt-4 mt-4 space-y-2 text-lg font-semibold border-t border-gray-400 dark:border-gray-700">
+
+            {/* Settings */}
+            <li>
+              <a href="/settings" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white group">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2">
+                  <path d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z"/>
+                  <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                </g>
+              </svg>
+                <span className="ml-3">Settings</span>
+              </a>
+            </li>
+
+          </ul>
+
         </div>
-      </div>
+      </aside>
+
     </>
   )
 }
@@ -204,12 +113,12 @@ export default function Layout({ children }: any) {
     <>
       <div className="flex">
 
-        <div className="flex items-baseline justify-center ml-6">
+        <div>
           <NavBar />
         </div>
 
         {/* main content */}
-        <div className="flex">
+        <div className="sm:ml-64">
           <main>
             {children}
           </main>
