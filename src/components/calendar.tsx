de22afaf6892949749ef7389e425c6ce3c72b0c7
atefkbenothman <atefkbenothman@gmail.com>
2023-06-22 13:21:46 -0700

    removed unused components and pages

--- src/components/calendar.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityCalendar } from "activity-calendar-react"
-
-export default function Calendar({ data }) {
-  return (
-    <div>
-      {/* <ActivityCalendar key={data} sampleData={data} showMonth={true} showDay={true} /> */}
-    </div>
-  )
-}
