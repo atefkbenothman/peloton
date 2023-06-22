32d247c875ce27609e60c964664fbd15fc47ca35
atefkbenothman <atefkbenothman@gmail.com>
2023-06-21 19:35:00 -0700

    clean up homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
     <>
-      <div className="h-screen mx-6 my-6">
+      <div className="min-h-screen mx-6 py-6">
         <div className="m-auto">
+
+          {/* Title */}
           <h3 className="text-3xl font-bold">
             Home
           </h3>
+
         </div>
       </div>
     </>
