04b5e5e0ab2155994bbd3f099b4fc9cae092269d
atefkbenothman <atefkbenothman@gmail.com>
2023-06-28 20:14:17 -0700

    format code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  env: {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
-    REDIS_URL: process.env.REDIS_URL
-  }
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
