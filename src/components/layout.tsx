3afeda97b0185b51c5173e61afc66a3a010afd52
atefkbenothman <atefkbenothman@gmail.com>
2023-08-02 21:58:04 -0700

    add search page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -158,6 +158,31 @@ function NavBar() {
               </Link>
             </li>
 
+            {/* Search */}
+            <li>
+              <Link
+                href="/search"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+              >
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Search</span>
+              </Link>
+            </li>
+
             {/* KOM */}
             <li>
               <Link
---
+++ src/pages/search.tsx
@@ -0,0 +1,140 @@
+import React from "react"
+import { useEffect } from "react"
+
+export default function Search() {
+  const [stravaAccessToken, setStravaAccessToken] = React.useState("")
+  const [date, setDate] = React.useState("")
+  const [activities, setActivities] = React.useState([])
+  const [numActivities, setNumActivities] = React.useState(0)
+  const [totalDistance, setTotalDistance] = React.useState(0)
+
+  useEffect(() => {
+    setStravaAccessToken(window.localStorage.getItem("accessToken") || "")
+  }, [])
+
+  const handleDateChange = (e: any) => {
+    const date = e.target.value
+    setDate(date)
+  }
+
+  const handleSearch = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault
+    if (!date) {
+      console.log("choose a date first!")
+      return
+    }
+    const baseActivitiesURL = "https://www.strava.com/api/v3/athlete/activities"
+    const dateToEpoch = (Date.parse(date) / 1000).toString()
+    let page = 1
+    let allActivities = []
+    try {
+      while (true) {
+        const params = new URLSearchParams({
+          after: dateToEpoch,
+          page: page.toString(),
+          per_page: "200"
+        })
+        const paramsString = params.toString()
+        const activitiesURL = `${baseActivitiesURL}?${paramsString}`
+        const res = await fetch(activitiesURL, {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + stravaAccessToken
+          }
+        })
+        const data = await res.json()
+        // check if there are still activities on this page
+        if (data.length === 0) {
+          // no more activities, break the loop
+          break
+        }
+        // filter activities with type "ride"
+        const rideActivities = data.filter(
+          (activity) => activity.type === "Ride"
+        )
+        // append activities
+        allActivities = [...allActivities, ...rideActivities]
+        // move to next page for next request
+        page++
+      }
+      console.log(allActivities)
+      setActivities(allActivities)
+      setNumActivities(allActivities.length)
+      // calculate total distance
+      const totalMiles = allActivities.reduce((total, activity) => {
+        return total + activity.distance
+      }, 0)
+      setTotalDistance(totalMiles)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <div>
+      <div className="min-h-screen mx-6 py-6">
+        <div className="m-auto">
+          {/* Profile Section */}
+          <div className="mb-10">
+            {/* Title */}
+            <p className="text-3xl font-bold text-black mb-4">Search</p>
+            {/* Search */}
+            <div className="flex items-start space-x-4">
+              <p>
+                How many miles since{" "}
+                <input
+                  type="date"
+                  id="date-picker"
+                  onChange={handleDateChange}
+                ></input>{" "}
+                ?
+              </p>
+              <button
+                className="btn mb-6 bg-green-500 text-white rounded p-2 shadow font-bold"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
+            </div>
+            {/* Data */}
+            <div>
+              <ol>
+                {/* Activities */}
+                {activities.map((activity, index) => (
+                  <li
+                    key={activity.id}
+                    className="grid grid-cols-3"
+                  >
+                    <div className="min-w-[200px]">
+                      {index + 1}.{" "}
+                      <a
+                        href={`https://www.strava.com/activities/${activity.id}`}
+                        className="text-blue-300"
+                      >
+                        {activity.name}
+                      </a>
+                    </div>
+                    <div className="min-w-[200px]">
+                      {new Date(activity.start_date).toLocaleDateString()}
+                    </div>
+                    <div className="min-w-[200px]">
+                      {(activity.distance / 1609.344).toFixed(2)} mi
+                    </div>
+                  </li>
+                ))}
+                {/* Total */}
+                <li className="grid grid-cols-3 font-bold text-lg">
+                  <div className="min-w-[200px]">Total: ({numActivities})</div>
+                  <div className="min-w-[200px]"></div>
+                  <div className="min-w-[200px]">
+                    {(totalDistance / 1609.344).toFixed(2)} mi
+                  </div>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
