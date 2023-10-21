ba819d110f508c4aae2c6ad9c89b2a52d4c5ad8f
atefkbenothman <atefkbenothman@gmail.com>
2023-10-20 21:58:56 -0700

    fix build issue

--- src/components/tabs/photos.tsx
+++ src/components/tabs/photos.tsx
@@ -9,7 +9,10 @@ export default function Photos({ photos }: { photos: any[] }) {
         <>
           <div className="grid grid-cols-4 gap-4">
             {photos.map((photo: any, idx) => (
-              <section className="bg-white shadow-md rounded-sm h-fit">
+              <section
+                key={idx}
+                className="bg-white shadow-md rounded-sm h-fit"
+              >
                 <div className="p-2">
                   <div className="mx-auto max-w-screen-sm text-center">
                     <Image
